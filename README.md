576768fa03c908698c7b861f3a307c113defc7ec
Andres Rubio <andres.rubio@artificial-solutions.com>
2024-03-12 23:32:15 +0100

    Initial commit
    Removes binary

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# AIMarkerspace-middleterm
+---
+title: AImarkerspace Week 1
+emoji: 🏢
+colorFrom: gray
+colorTo: red
+sdk: docker
+pinned: false
+license: openrail
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+
+## How to build
+docker build -t middleterm .
+
+## How to run in local
+docker run --name middleterm -p 7860:7860 middleterm
---
+++ app.py
@@ -0,0 +1,102 @@
+# You can find this code for Chainlit python streaming here (https://docs.chainlit.io/concepts/streaming/python)
+
+# OpenAI Chat completion
+import os
+import requests
+import chainlit as cl  # importing chainlit for our app
+from dotenv import load_dotenv
+
+import llama_index
+from llama_index.core import set_global_handler
+
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.core import Settings
+from llama_index.core import (
+    SimpleDirectoryReader,
+    load_index_from_storage,
+    VectorStoreIndex,
+    StorageContext,
+)
+
+from llama_index.vector_stores.faiss import FaissVectorStore
+import faiss
+
+# ChatOpenAI Templates
+system_template = """You are a helpful assistant who always speaks in a pleasant tone!
+"""
+
+user_template = """{input}
+Think through your response step by step.
+"""
+# query_engine = index.as_query_engine()
+# response = query_engine.query("Who is the E-VP, Operations - and how old are they?")
+# print(response.response)
+#
+# response = query_engine.query("What is the gross carrying amount of Total Amortizable Intangible Assets for Jan 29, 2023?")
+# print(response.response)
+# if storage folder exists and is not empty, load the index from it else from documents
+
+
+@cl.on_chat_start  # marks a function that will be executed at the start of a user session
+async def start_chat():
+    load_dotenv()
+
+    set_global_handler("wandb", run_args={"project": "aie1-llama-index-middleterm"})
+    wandb_callback = llama_index.core.global_handler
+
+    Settings.llm = OpenAI(temperature=0.1, model="gpt-3.5-turbo")
+    Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small")
+
+    index = None
+    if os.path.exists("./storage") and os.listdir("./storage"):
+        vector_store = FaissVectorStore.from_persist_dir("./storage")
+        storage_context = StorageContext.from_defaults(
+            vector_store=vector_store, persist_dir="./storage"
+        )
+        index = load_index_from_storage(storage_context=storage_context)
+    else:
+        with requests.get('https://d18rn0p25nwr6d.cloudfront.net/CIK-0001045810/1cbe8fe7-e08a-46e3-8dcc-b429fc06c1a4.pdf', stream=True) as r:
+            r.raise_for_status()  # Raises a HTTPError if the response status code is 4XX/5XX
+            with open('nvidia_data/paper.pdf', 'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    # If you have chunk encoded response uncomment if
+                    # and set chunk_size parameter to None.
+                    # if chunk:
+                    f.write(chunk)
+        documents = SimpleDirectoryReader("nvidia_data/").load_data()
+        faiss_index = faiss.IndexFlatL2(1536)
+        storage_context = StorageContext.from_defaults(vector_store=FaissVectorStore(faiss_index=faiss_index))
+        index = VectorStoreIndex.from_documents(
+            documents,
+            storage_context=storage_context,
+            persist_dir="./storage"
+        )
+
+    cl.user_session.set("wandb_callback", wandb_callback)
+    cl.user_session.set("query_engine", index.as_query_engine())
+
+
+@cl.on_message  # marks a function that should be run each time the chatbot receives a message from a user
+async def main(message: cl.Message):
+    query_engine = cl.user_session.get("query_engine")
+    template = (f"You are a helpful assistant who always speaks in a pleasant tone! responds to user input with a step by step guide using this context: {message.content} input: {input}")
+    response = query_engine.query(template)
+
+    response_message = cl.Message(content="")
+    for token in response.response:
+        await response_message.stream_token(token=token)
+
+    await response_message.send()
+
+
+@cl.on_stop
+def on_stop():
+    print("The user wants to stop the task!")
+    cl.user_session.get("wandb_callback").finish()
+
+
+@cl.on_chat_end
+def on_chat_end():
+    print("The user disconnected!")
+    cl.user_session.get("wandb_callback").finish()
